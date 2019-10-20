e2ed83dae88e177cae5e89923d3a46370e4dcfd8
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2019-10-20 18:14:06 +0200

    klase Korpa i supermarket sa metodama za dodavanje i izbacivanje artikala

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,35 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] artikli = new Artikl[50];
+    private int brojArtikala = 0;
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public boolean dodajArtikl(Artikl a) {
+        if(brojArtikala == 50){
+            return false;
+        }
+
+        artikli[brojArtikala] = a;
+        brojArtikala++;
+        return true;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i = 0; i < brojArtikala; i++){
+            if(artikli[i].getKod().equals(kod)){
+                Artikl izbacen = artikli[i];
+                for(int j = i; j < brojArtikala - 1; j++){
+                    artikli[j] = artikli[j-1];
+                }
+                brojArtikala--;
+                return izbacen;
+            }
+        }
+
+        return null;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,31 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] artikli = new Artikl[1000];
+    private int brojArtikala = 0;
+
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i = 0; i < brojArtikala; i++){
+            if(artikli[i].getKod().equals(kod)){
+                Artikl izbacen = artikli[i];
+                for(int j = i; j < brojArtikala - 1; j++){
+                    artikli[j] = artikli[j-1];
+                }
+                brojArtikala--;
+                return izbacen;
+            }
+        }
+
+        return null;
+    }
+
+    public void dodajArtikl(Artikl a) {
+        artikli[brojArtikala] = a;
+        brojArtikala++;
+    }
 }
